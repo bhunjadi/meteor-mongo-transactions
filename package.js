c062f0b2cbaa38ebd6ef2402749aeab98587479e
Bero <bero@stillwater.dev>
2024-11-09 18:07:26 +0100

    version bump to 2.1.0

--- package.js
+++ package.js
@@ -1,16 +1,16 @@
 Package.describe({
   name: 'bhunjadi:mongo-transactions',
-  version: '2.0.0',
+  version: '2.1.0',
   // Brief, one-line summary of the package.
   summary: '',
   // URL to the Git repository containing the source code for this package.
   git: '',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
-  documentation: 'README.md'
+  documentation: 'README.md',
 });
 
-Package.onUse(function(api) {
+Package.onUse(function (api) {
   api.versionsFrom('2.6');
   api.use('typescript');
   api.use('promise');
@@ -18,7 +18,7 @@ Package.onUse(function(api) {
   api.mainModule('src/index.ts', 'server');
 });
 
-Package.onTest(function(api) {
+Package.onTest(function (api) {
   api.use('typescript');
   api.use('bhunjadi:mongo-transactions');
   api.use('mongo');
